5170b659fa7524ca577a78b01bd103690d5648d4
satishsingh003 <satishsinghoct87@gmail.com>
2022-02-06 23:28:57 +0530

    group Anagrams in JS

---
+++ groupAnagrams.js
@@ -0,0 +1,25 @@
+let collectAnagrams = (words) => {
+    let anagrams = {}
+    let collectedAnagrams = []
+
+    for (let word of words){
+        // alphabetize letters
+        let letters = word.split('').sort().join('')
+
+        // create hash key-value pair for alphabetized letters of word if it does not exist
+        anagrams[letters] = anagrams[letters] || []
+        
+        // add word to value of the key which matches its letters
+        anagrams[letters].push(word)
+        }
+    
+    // iterate through anagrams hash keys and add their values as subarrays of the collectedAnagrams array
+    for (let key in anagrams){
+        collectedAnagrams.push(anagrams[key])
+    }
+
+    // return the array of arrays
+    return collectedAnagrams
+}
+
+module.exports = collectAnagrams
