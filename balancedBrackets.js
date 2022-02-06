372af4a248c76d6d97eaa55ea846837b4071f2fa
satishsingh003 <satishsinghoct87@gmail.com>
2022-02-06 23:26:23 +0530

    Balanced Brackets

---
+++ balancedBrackets.js
@@ -0,0 +1,26 @@
+let isBalanced = (input) => {
+
+    let brackets = "[]{}()<>"
+    let stack = []
+  
+    for(let bracket of input) {
+      let bracketsIndex = brackets.indexOf(bracket)
+  
+      if (bracketsIndex === -1){
+        continue
+      }
+  
+      if(bracketsIndex % 2 === 0) {
+        stack.push(bracketsIndex + 1)
+      } else {
+        if(stack.length === 0 || stack.pop() !== bracketsIndex) {
+          return false;
+        }
+      }
+    }
+    return stack.length === 0
+  }
+  
+  module.exports = isBalanced
+
+  ///https://levelup.gitconnected.com/solving-balanced-brackets-in-javascript-with-stacks-edbc52a57309
